a8445657caac6489fb1b437fc6a54321347c81e6
Patrick Van Weelden <pvanweel@yahoo.com>
2022-05-08 18:45:28 -0500

    styled the landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,7 @@ import Lottie from 'react-lottie';
 import animationData from '../animations/landinganimation/data.js';
 import ButtonArrow from '../components/ui/ButtonArrow';
 
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, useTheme } from '@material-ui/styles';
 import { Grid, Button, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
@@ -14,10 +14,56 @@ const useStyles = makeStyles(theme => ({
     marginTop: '2em',
     marginLeft: '10%',
   },
+  estimateButton: {
+    ...theme.typography.estimate,
+    backgroundColor: theme.palette.common.orange,
+    borderRadius: 50,
+    height: 45,
+    width: 145,
+    marginRight: 40,
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+    },
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '30em',
+    },
+  },
+  buttonContainer: {
+    marginTop: '1em',
+  },
+  learnButtonHero: {
+    borderColor: theme.palette.common.blue,
+    color: theme.palette.common.blue,
+    borderWidth: 2,
+    textTransform: 'none',
+    borderRadius: 50,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: '0.9em',
+    height: 45,
+    width: 145,
+  },
+  mainContainer: {
+    marginTop: '5em',
+    [theme.breakpoints.down('md')]: {
+      marginTop: '3em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginTop: '2em',
+    },
+  },
+  heroTextContainer: {
+    minWidth: '21.5em',
+    marginLeft: '1em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
 }));
 
 const LandingPage = () => {
   const classes = useStyles();
+  const theme = useTheme();
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -27,7 +73,7 @@ const LandingPage = () => {
     },
   };
   return (
-    <Grid container direction="column">
+    <Grid container direction="column" className={classes.mainContainer}>
       <Grid item>
         <Grid
           container
@@ -35,20 +81,30 @@ const LandingPage = () => {
           justifyContent="flex-end"
           alignItems="center"
         >
-          <Grid sm item>
+          <Grid sm item className={classes.heroTextContainer}>
             <Typography variant="h2" align="center">
               Bringing West Coast Technology
               <br />
               to the Midwest
             </Typography>
-            <Grid container>
+            <Grid
+              container
+              justifyContent="center"
+              className={classes.buttonContainer}
+            >
               <Grid item>
-                <Button variant="contained">Free Estimate</Button>
+                <Button className={classes.estimateButton} variant="contained">
+                  Free Estimate
+                </Button>
               </Grid>
               <Grid item>
-                <Button variant="outlined">
-                  Learn More
-                  <ButtonArrow width={15} height={15} fill="red"></ButtonArrow>
+                <Button variant="outlined" className={classes.learnButtonHero}>
+                  <span style={{ marginRight: 10 }}>Learn More</span>
+                  <ButtonArrow
+                    width={15}
+                    height={15}
+                    fill={theme.palette.common.blue}
+                  ></ButtonArrow>
                 </Button>
               </Grid>
             </Grid>
