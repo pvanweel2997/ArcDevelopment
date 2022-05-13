4613294da25a2bc4a6691f9bed4841ad8ceeb0db
Patrick Van Weelden <pvanweel@yahoo.com>
2022-05-13 18:00:56 -0500

    work on the custom software page

--- src/components/ui/CustomSoftware.js
+++ src/components/ui/CustomSoftware.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { makeStyles, useTheme } from '@material-ui/styles';
-import {
-  Grid,
-  Button,
-  Typography,
-  useMediaQuery,
-  IconButton,
-} from '@material-ui/core';
+import Lottie from 'react-lottie';
+import { makeStyles } from '@material-ui/styles';
+import { Grid, Typography, IconButton } from '@material-ui/core';
 import backArrow from '../../assets/backArrow.svg';
 import forwardArrow from '../../assets/forwardArrow.svg';
 import lightbulb from '../../assets/bulb.svg';
 import cash from '../../assets/cash.svg';
 import stopwatch from '../../assets/stopwatch.svg';
+
+import documentsAnimation from '../../animations/documentsAnimation/data.js';
+import scaleAnimation from '../../animations/scaleAnimation/data';
 
 const useStyles = makeStyles(theme => ({
   heading: {
@@ -27,12 +25,32 @@ const useStyles = makeStyles(theme => ({
     paddingTop: '2em',
     paddingBottom: '10em',
   },
+  itemContainer: {
+    maxWidth: '40em',
+  },
 }));
 
 const CustomSoftware = ({ setValue, setSelectedIndex }) => {
   const classes = useStyles();
-  const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const documentsOptions = {
+    loop: true,
+    autoplay: false,
+    documentsAnimation,
+    renderSettings: {
+      preserveAspectRation: 'xMidYMid slice',
+    },
+  };
+
+  const scaleOptions = {
+    loop: true,
+    autoplay: false,
+    scaleAnimation,
+    renderSettings: {
+      preserveAspectRation: 'xMidYMid slice',
+    },
+  };
+
   return (
     <Grid container direction="column" className={classes.mainContainer}>
       <Grid item container direction="row">
@@ -146,6 +164,62 @@ const CustomSoftware = ({ setValue, setSelectedIndex }) => {
           </Grid>
           <Grid item>
             <img src={cash} alt="cash" />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container direction="row">
+        <Grid item container className={classes.itemContainer}>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4">Digital Documents & Data</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                Reduce Errors. Reduce Waste. Reduce Costs.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                Billions are spent annually on the purchasing, printing, and
+                distribution of paper. On top of the massive environmental
+                impact this has, it causes harm to your bottom line as well.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                By utilizing digital forms and documents you can remove these
+                obsolete expenses, accelerate your communication, and help the
+                Earth.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentsOptions}
+              style={{ maxHeight: 275, maxWidth: 275, minHeight: 250 }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item md>
+            <Lottie
+              options={scaleOptions}
+              style={{ maxHeight: 260, maxWidth: 280 }}
+            />
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4" align="right">
+                Scale
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="right" paragraph>
+                Whether you’re a large brand, just getting started, or taking
+                off right now, our application architecture ensures pain-free
+                growth and reliability.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
