44fecf65365350166eef78d0e5043559fae4d1c1
Patrick Van Weelden <pvanweel@yahoo.com>
2022-05-24 16:09:20 -0500

    working on contact us

--- src/components/ui/Contact.js
+++ src/components/ui/Contact.js
@@ -1,7 +1,80 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import {
+  Grid,
+  Typography,
+  useMediaQuery,
+  TextField,
+  Button,
+} from '@material-ui/core';
 
-const Contact = () => {
-  return <div>contact us</div>;
+import background from '../../assets/background.jpg';
+import phoneIcon from '../../assets/phone.svg';
+import emailIcon from '../../assets/email.svg';
+
+const useStyles = makeStyles(theme => ({
+  background: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '60em',
+  },
+}));
+
+const Contact = ({ setValue, setSelectedIndex }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <Grid container direction="row">
+      <Grid item container direction="column" justifyContent="center" lg={3}>
+        <Grid item>
+          <Typography variant="h2" style={{ lineHeight: 1 }}>
+            Contact Us
+          </Typography>
+          <Typography
+            vaiant="body1"
+            style={{ color: theme.palette.common.blue }}
+          >
+            We're waiting.
+          </Typography>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <img src={phoneIcon} alt="phone" style={{ marginRight: '0.5em' }} />
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{ color: theme.palette.common.blue, fontSize: '1rem' }}
+            >
+              555-555-5555
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <img
+              src={emailIcon}
+              alt="envelope"
+              style={{ marginRight: '0.5em', verticalAlign: 'bottom' }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{ color: theme.palette.common.blue, fontSize: '1rem' }}
+            >
+              test@test.com
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container className={classes.background} lg={9}></Grid>
+    </Grid>
+  );
 };
 
 export default Contact;
